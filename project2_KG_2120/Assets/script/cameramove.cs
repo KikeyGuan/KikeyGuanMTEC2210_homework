0edd910308c8e70b57844f1db688a8c94c23d0ca
KikeyGuan <Kikey.Guan@mail.citytech.cuny.edu>
2024-03-01 02:53:14 -0500

    copy of work. added movement

---
+++ project2_KG_2120/Assets/script/cameramove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// help from: https://www.youtube.com/watch?v=f473C43s8nE&t=200s&ab_channel=Dave%2FGameDevelopment
+
+public class cameramove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float sensX;
+    public float sensY;
+    public Transform orientation;
+    float xRotation;
+    float yRotation;
+    void Start()
+    {
+        //lock mouse in place and trun invisi
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-90f,90f);
+        //rotate cam
+        transform.rotation = Quaternion.Euler (xRotation,yRotation,0);
+        orientation.rotation = Quaternion.Euler(0, yRotation,0);
+    }
+}
---
+++ project2_KG_2120/Assets/script/playermove.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//taken from unity scripting API: character controller move
+public class playermove : MonoBehaviour
+{
+    
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 6f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    Rigidbody rb;
+    public Transform orientation;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        playerVelocity = new Vector3(0,2.0f,0);
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (controller.isGrounded)
+        {
+            Debug.Log ("grounded");
+        }
+        if (groundedPlayer && playerVelocity.y > 0)
+        {
+            Debug.Log ("that one if statement");
+            //playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            Debug.Log ("jumped");
+            playerVelocity.y = playerVelocity.y + 2f;
+            //transform.position = transform.position + new Vector3(0,jumpHeight*playerSpeed*Time.deltaTime,0);
+
+            //playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            //rb.AddForce(playerVelocity * jumpHeight, ForceMode.Impulse);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+    
+    //public float moveSpeed;
+
+}
+
+
